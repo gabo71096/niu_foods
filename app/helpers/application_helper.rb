2af0a390d726bc950252d1762fcb8c36e240926e
Gabriel Lopez <gabo_71096@hotmail.com>
2024-05-27 13:16:22 -0400

    Chore: Show updated_at time in local time and formatted

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,11 @@
 
 # ApplicationHelper module
 module ApplicationHelper
+  def local_time(datetime, time_zone = 'Santiago')
+    datetime.in_time_zone(time_zone)
+  end
+
+  def format_datetime(datetime)
+    datetime.strftime('%H:%M:%S %d/%m/%Y')
+  end
 end
