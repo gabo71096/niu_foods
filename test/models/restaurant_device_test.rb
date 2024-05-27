2fdb4ddd1bfc0448137fef41b9e2accd3bc50a6e
Gabriel Lopez <gabo_71096@hotmail.com>
2024-05-27 19:35:56 -0400

    Test: Create RestaurantDevice tests

--- test/models/restaurant_device_test.rb
+++ test/models/restaurant_device_test.rb
@@ -3,7 +3,23 @@
 require 'test_helper'
 
 class RestaurantDeviceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save Restaurant Device without status' do
+    restaurant_device = RestaurantDevice.new(restaurant: Restaurant.first, status: nil, device_type: :printer)
+    assert_not(restaurant_device.save)
+  end
+
+  test 'should not save Restaurant Device without device type' do
+    restaurant_device = RestaurantDevice.new(restaurant: Restaurant.first, status: :ok, device_type: nil)
+    assert_not(restaurant_device.save)
+  end
+
+  test 'should not save Restaurant Device without restaurant' do
+    restaurant_device = RestaurantDevice.new(status: :ok, device_type: :printer)
+    assert_not(restaurant_device.save)
+  end
+
+  test 'should save Restaurant Device correctly' do
+    restaurant_device = RestaurantDevice.new(restaurant: Restaurant.first, status: :ok, device_type: :printer)
+    assert(restaurant_device.save)
+  end
 end
