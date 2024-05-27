fcc7f99f7183c816cac7b74a21a1917cc5f6a933
Gabriel Lopez <gabo_71096@hotmail.com>
2024-05-27 18:09:37 -0400

    Feat: Add model validations and normalizations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,6 @@
 # Restaurant model
 class Restaurant < ApplicationRecord
   has_many :restaurant_devices, dependent: :destroy
+
+  normalizes :name, with: ->(name) { name.strip.titlecase }
 end
--- app/models/restaurant_device.rb
+++ app/models/restaurant_device.rb
@@ -5,4 +5,10 @@ class RestaurantDevice < ApplicationRecord
   belongs_to :restaurant
 
   enum :device_type, %i[printer web_server db_server]
+  enum :status, %i[ok error]
+
+  validates :device_type, presence: true
+  validates :status, presence: true
+
+  normalizes :name, with: ->(name) { name.strip.titlecase }
 end
